1eddab3f609e7e651316d8eb4efbdca9d157dd0c
sebastianariza <50971923+sebastianariza@users.noreply.github.com>
2019-05-31 16:05:41 -0500

    Create header_director.php

---
+++ header_director.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>UCC</title>
+        <link href="../css/general_style.css" type="text/css" rel="stylesheet">
+        <link href="../css/bootstrap.css" type="text/css" rel="stylesheet">
+        <link href="../css/jquery-ui-1.8.16.custom.css" type="text/css" rel="stylesheet">
+        <link href="../css/bootstrap.css" type="text/css" rel="stylesheet">
+        <link href="../css/nav.css" type="text/css" rel="stylesheet">
+        <link href="../js/jquery-ui.css" rel="stylesheet" type="text/css">
+        <link href="../js/jquery-ui.min.css" rel="stylesheet" type="text/css">
+        <link href="../css/formulario.css"  type="text/css" rel="stylesheet"> 
+        
+        <meta charset="utf-8">
+    </head>
+    
+   
+                  
+           
+    </div>
+    </div>
+    <div id="header">
+                
+        </ul>
+    </div>
+   
+    <br>
+
+
+
+
+</head>
+
+
+
+</body>
+</html>
+</html>
