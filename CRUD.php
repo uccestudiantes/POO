74bc2ab271c8fa5a0a2fbe4e0b971b5a84d150b6
ingluismartinez <53498947+ingluismartinez@users.noreply.github.com>
2019-08-01 21:05:46 -0500

    Create CRUD.php

---
+++ CRUD.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <title>Pricing example · Bootstrap</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/pricing/">
+
+    <!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="pricing.css" rel="stylesheet">
+  </head>
+  <body>
+
+<div class="container">
+  <div class="card-deck mb-3 text-center">
+    
+<div class="card mb-3 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">Crear</h4>
+      </div>
+      <div class="card-body">
+
+        <button type="button" class="btn btn-lg btn-block btn-primary">Crear</button>
+      </div>
+    </div>
+    <div class="card mb-3 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">Actualizar</h4>
+      </div>
+      <div class="card-body">
+
+        <button type="button" class="btn btn-lg btn-block btn-primary">Actualizar</button>
+      </div>
+    </div>
+    <div class="card mb-3 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">Consultar</h4>
+      </div>
+      <div class="card-body">
+
+        <button type="button" class="btn btn-lg btn-block btn-primary">Consultar</button>
+      </div>
+    </div>
+    <div class="card mb-3 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">Borrar</h4>
+      </div>
+      <div class="card-body">
+
+        <button type="button" class="btn btn-lg btn-block btn-primary">Borrar</button>
+      </div>
+    </div>
+  </div>
+  
+</div>
+</body>
+</html>
